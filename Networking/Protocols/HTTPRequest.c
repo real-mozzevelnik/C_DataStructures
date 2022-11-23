50497400e234265864f9a1b4807706cc34da20b4
Vadim Chumachenko <vadimchumachenko123@gmail.com>
2022-11-23 21:49:10 +0300

    bug fixes in httprequest

--- Networking/Protocols/HTTPRequest.c
+++ Networking/Protocols/HTTPRequest.c
@@ -10,12 +10,9 @@ void extract_body(struct HTTPRequest *reauest, char *body);
 
 struct HTTPRequest http_request_constructor(char *request_string)
 {
-    printf("1\n");
     struct HTTPRequest request;
 
-    // char *requested = (char*)malloc((strlen(request_string) + 1) * sizeof(char));
-    // forgive me for that
-    char requested[strlen(request_string)];
+    char *requested = (char*)malloc(strlen(request_string)+1);
     strcpy(requested, request_string);
 
     int cycle_var = strlen(requested) - 2;
@@ -35,6 +32,7 @@ struct HTTPRequest http_request_constructor(char *request_string)
     extract_header_fields(&request, header_fields);
     extract_body(&request, body);
 
+    free(requested);
     return request;
 }
 
@@ -47,18 +45,20 @@ void http_request_destructor(struct HTTPRequest *request)
 
 void extract_request_line_fields(struct HTTPRequest *request, char *request_line)
 {
-    //forgive me
-    char fields[strlen(request_line)];
+    char *fields = (char*)malloc(strlen(request_line)+1);
     strcpy(fields, request_line);
 
     char *method = strtok(fields, " ");
     char *uri = strtok(NULL, " ");
     char *http_version = strtok(NULL, "\0");
+    http_version = strtok(http_version, "/");
+    http_version = strtok(NULL, "/");
+    float http_ver = (float)atof(http_version);
 
     struct Dictionary request_line_dict = dictionary_constructor(compare_string_keys);
-    request_line_dict.insert(&request_line_dict, "method", sizeof("method"), method, sizeof(char[strlen(method)]));
-    request_line_dict.insert(&request_line_dict, "uri", sizeof("uri"), uri, sizeof(char[strlen(uri)]));
-    request_line_dict.insert(&request_line_dict, "http_version", sizeof("http_version"), http_version, sizeof(char[strlen(http_version)]));
+    request_line_dict.insert(&request_line_dict, "method", sizeof("method"), method, sizeof(char)*strlen(method)+1);
+    request_line_dict.insert(&request_line_dict, "uri", sizeof("uri"), uri, sizeof(char)*strlen(uri)+1);
+    request_line_dict.insert(&request_line_dict, "http_version", sizeof("http_version"), &http_ver, sizeof(http_ver));
 
     request->request_line = request_line_dict;
     
@@ -66,18 +66,19 @@ void extract_request_line_fields(struct HTTPRequest *request, char *request_line
     {
         extract_body(request, (char *)request->request_line.search(&request->request_line, "uri", sizeof("uri")));
     }
+    free(fields);
 }
 
 void extract_header_fields(struct HTTPRequest *request, char *header_fields)
 {
-    char fields[strlen(header_fields)];
+    char *fields = (char*)malloc(strlen(header_fields)+1);
     strcpy(fields, header_fields);
 
     struct Queue headers = queue_constructor();
     char *field = strtok(fields, "\n");
     while (field)
     {
-        headers.push(&headers, field, sizeof(char[strlen(field)]));
+        headers.push(&headers, field, sizeof(char)*strlen(field)+1);
         field = strtok(NULL, "\n");
     }
 
@@ -87,6 +88,7 @@ void extract_header_fields(struct HTTPRequest *request, char *header_fields)
     while (header)
     {
         char *key = strtok(header, ":");
+        // printf("\n%s", key);
         char *value = strtok(NULL, "\0");
 
         if (value)
@@ -94,16 +96,19 @@ void extract_header_fields(struct HTTPRequest *request, char *header_fields)
             if (value[0] == ' ')
                 value++;
             
-            request->header_fields.insert(&request->header_fields, key, sizeof(char[strlen(key)]), value, sizeof(char[strlen(value)]));
+            request->header_fields.insert(&request->header_fields, key, sizeof(char)*strlen(key)+1, value, sizeof(char)*strlen(value)+1);
         }
         headers.pop(&headers);
         header = (char *)headers.peek(&headers);
     }
     queue_destructor(&headers);
+    free(fields);
 }
 
 void extract_body(struct HTTPRequest *request, char *body)
 {
+    char *body_to_extract = (char*)malloc(strlen(body)+1);
+    strcpy(body_to_extract, body);
     char *content_type = (char *)request->header_fields.search(&request->header_fields, "Content-Type", sizeof("Content-Type"));
     if (content_type)
     {
@@ -111,10 +116,11 @@ void extract_body(struct HTTPRequest *request, char *body)
         if (strcmp(content_type, "application/x-www-form-urlencoded") == 0)
         {
             struct Queue fields = queue_constructor();
-            char *field = strtok(body, "&");
+            char *field = strtok(body_to_extract, "&");
             while (field)
             {
-                fields.push(&fields, field, sizeof(char[strlen(field)]));
+                fields.push(&fields, field, sizeof(char)*strlen(field)+1);
+                field = strtok(NULL, "&");
             }
             field = fields.peek(&fields);
             while (field)
@@ -125,7 +131,7 @@ void extract_body(struct HTTPRequest *request, char *body)
                 if (value[0] == ' ')
                     value++;
                 
-                body_fields.insert(&body_fields, key, sizeof(char[strlen(key)]), value, sizeof(char[strlen(value)]));
+                body_fields.insert(&body_fields, key, sizeof(char)*strlen(key)+1, value, sizeof(char)*strlen(value)+1);
 
                 fields.pop(&fields);
                 field = fields.peek(&fields);
@@ -134,8 +140,9 @@ void extract_body(struct HTTPRequest *request, char *body)
         }
         else
         {
-            body_fields.insert(&body_fields, "data", sizeof("data"), body, sizeof(char[strlen(body)]));
+            body_fields.insert(&body_fields, "data", sizeof("data"), body_to_extract, sizeof(char)*strlen(body)+1);
         }
         request->body = body_fields;
     }
+    free(body_to_extract);
 }
--- Networking/Protocols/t.c
+++ Networking/Protocols/t.c
@@ -4,9 +4,10 @@
 
 int main()
 {
-    struct HTTPRequest req = http_request_constructor("POST /cgi-bin/process.cgi HTTP/1.1\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\nHost: www.tutorialspoint.com\nContent-Type: application/x-www-form-urlencoded\nContent-Length: length\nAccept-Language: en-us\nAccept-Encoding: gzip, deflate\nConnection: Keep-Alive\n\nlicenseID=string&content=string&/paramsXML=string");
-    float *h = (float*)req.request_line.search(&(req.request_line), "http_version", sizeof("http_version"));
-    // printf("%lf", *h);
+    struct HTTPRequest req = http_request_constructor("GET /cgi-bin/process.cgi HTTP/1.1\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\nHost: www.tutorialspoint.com\nContent-Type: application/x-www-form-urlencoded\nContent-Length: length\nAccept-Language: en-us\nAccept-Encoding: gzip, deflate\nConnection: Keep-Alive\n\nlicenseID=string&content=string&/paramsXML=string");
+    printf("\n\n%s\n", (char*)req.body.search(&req.body, "data", sizeof("data")));
+
+    http_request_destructor(&req);
     return 0;
 }
 

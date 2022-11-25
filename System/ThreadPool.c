139fdf8dfcaede945bbf2b75ba9136cc4f1d9026
Vadim Chumachenko <vadimchumachenko123@gmail.com>
2022-11-25 23:09:42 +0300

    Done ThreadPool, dont sure it works

--- System/ThreadPool.c
+++ System/ThreadPool.c
@@ -1,36 +1,84 @@
 #include "ThreadPool.h"
 #include <stdlib.h>
 
-void * thread_job(void *arg);
+void * generic_thread_function(void *arg);
+void add_work(struct ThreadPool *thread_pool, struct ThreadJob job);
 
 struct ThreadPool thread_pool_constructor(int num_threads)
 {
     struct ThreadPool thread_pool;
 
     thread_pool.num_threads = num_threads;
+
     thread_pool.active = 1;
-    thread_pool.pool = (pthread_t*)malloc(sizeof(pthread_t)*num_threads);
-    for (int i = 0; i < num_threads; i++)
-    {
-        pthread_create(&thread_pool.pool[i], NULL, thread_job, NULL);
-    }
+
     thread_pool.work = queue_constructor();
 
     thread_pool.lock = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
 
     thread_pool.signal = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
 
+    pthread_mutex_lock(&thread_pool.lock);
+
+    thread_pool.pool = (pthread_t*)malloc(sizeof(pthread_t)*num_threads);
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_create(&thread_pool.pool[i], NULL, generic_thread_function, &thread_pool);
+    }
+    
+    pthread_mutex_unlock(&thread_pool.lock);
+
+    thread_pool.add_work = add_work;
+
     return thread_pool;
 }
 
-void * thread_job(void *arg)
+void thread_pool_destructor(struct ThreadPool *thread_pool)
+{
+    thread_pool->active = 0;
+    for (int i = 0; i < thread_pool->num_threads; i++)
+    {
+        pthread_cond_signal(&thread_pool->signal);
+    }
+    for (int i = 0; i < thread_pool->num_threads; i++)
+    {
+        pthread_join(thread_pool->pool[i], NULL);
+    }
+    free(thread_pool->pool);
+    queue_destructor(&thread_pool->work);
+}
+
+struct ThreadJob thread_job_constructor(void * (*job_function)(void *arg), void *arg)
+{
+    struct ThreadJob job;
+    job.job = job_function;
+    job.arg = arg;
+    return job;
+}
+
+void * generic_thread_function(void *arg)
 {
     struct ThreadPool *thread_pool = (struct ThreadPool*)arg;
     while (thread_pool->active == 1)
     {
         pthread_mutex_lock(&thread_pool->lock);
+        pthread_cond_wait(&thread_pool->signal, &thread_pool->lock);
         struct ThreadJob job = *(struct ThreadJob*)thread_pool->work.peek(&thread_pool->work);
         thread_pool->work.pop(&thread_pool->work);
         pthread_mutex_unlock(&thread_pool->lock);
+        if (job.job)
+        {
+            job.job(job.arg);
+        }
     }
+    return NULL;
+}
+
+void add_work(struct ThreadPool *thread_pool, struct ThreadJob job)
+{
+    pthread_mutex_lock(&thread_pool->lock);
+    thread_pool->work.push(&thread_pool->work, &job, sizeof(job));
+    pthread_cond_signal(&thread_pool->signal);
+    pthread_mutex_unlock(&thread_pool->lock);
 }
--- System/ThreadPool.h
+++ System/ThreadPool.h
@@ -4,6 +4,12 @@
 #include "../DataStructures/Lists/Queue.h"
 #include <pthread.h>
 
+struct ThreadJob
+{
+    void * (*job)(void *arg);
+    void *arg;
+};
+
 struct ThreadPool
 {
     int num_threads;
@@ -12,15 +18,13 @@ struct ThreadPool
     pthread_t *pool;
     pthread_mutex_t lock;
     pthread_cond_t signal;
-};
 
-struct ThreadJob
-{
-    void * (*job)(void *arg);
-    void *arg;
+    void (*add_work)(struct ThreadPool *thread_pool, struct ThreadJob job);
 };
 
 struct ThreadPool thread_pool_constructor(int num_threads);
-struct ThreadJob thread_job_constructor(void * (*job)(void *arg), void *arg);
+struct ThreadJob thread_job_constructor(void * (*job_function)(void *arg), void *arg);
+
+void thread_pool_destructor(struct ThreadPool *thread_pool);
 
 #endif
